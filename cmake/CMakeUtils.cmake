2dbe25069f4b19d2f99ef2f96c802e173866e3ca
R1tschY <r1tschy@yahoo.de>
2016-04-01 13:42:25 +0200

    outsourced cmake modules

--- cmake/CMakeUtils.cmake
+++
@@ -1,9 +0,0 @@
-
-
-macro(append _var _arg)
-	set(${_var} "${${_var}}${_arg}" ${ARGN})
-endmacro()
-
-macro(move_to_parent _var)
-	set(${_var} "${${_var}}" PARENT_SCOPE)
-endmacro()
--- cmake/ListUtils.cmake
+++
@@ -1,5 +0,0 @@
-
-
-macro(append _var _arg)
-	set(_var "${_var} ${_arg}" ${ARGN})
-endmacro()
