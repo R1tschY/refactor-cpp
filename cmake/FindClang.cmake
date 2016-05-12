f48fc36bd9fe628e6acefdabce1b477875a12a55
R1tschY <r1tschy@yahoo.de>
2016-05-13 00:22:39 +0200

    improved cmake modules for LLVM and Clang

--- cmake/FindClang.cmake
+++ cmake/FindClang.cmake
@@ -1,59 +1,31 @@
-# Find Clang
-# Copyright (c) 2012 Canal Team
-# 
-# It defines the following variables
-# CLANG_FOUND        - True if Clang found.
-# CLANG_INCLUDE_DIRS - where to find Clang include files
-# CLANG_LIBS         - list of clang libs
+include(FindPackageHandleStandardArgs)
 
-if (NOT LLVM_INCLUDE_DIRS OR NOT LLVM_LIBRARY_DIRS)
-   message(FATAL_ERROR "No LLVM and Clang support requires LLVM")
-else (NOT LLVM_INCLUDE_DIRS OR NOT LLVM_LIBRARY_DIRS)
+if (NOT LLVM_FOUND)
+   message(FATAL_ERROR "Clang support requires LLVM and no LLVM Support")
+   return()
+endif()
 
-MACRO(FIND_AND_ADD_CLANG_LIB _libname_)
-find_library(CLANG_${_libname_}_LIB ${_libname_} ${LLVM_LIBRARY_DIRS} ${CLANG_LIBRARY_DIRS})
-if (CLANG_${_libname_}_LIB)
-   set(CLANG_LIBS ${CLANG_LIBS} ${CLANG_${_libname_}_LIB})
-endif (CLANG_${_libname_}_LIB)
-ENDMACRO(FIND_AND_ADD_CLANG_LIB)
+# components
+if (Clang_FIND_COMPONENTS)
+  foreach (_comp ${Clang_FIND_COMPONENTS})
+    set(_libname_ "clang${_comp}")
+    find_library(Clang_${_libname_}_LIB ${_libname_} ${LLVM_LIBRARY_DIRS} ${Clang_LIBRARY_DIRS})
+    if (Clang_${_libname_}_LIB)
+       set(Clang_LIBS ${Clang_LIBS} ${Clang_${_libname_}_LIB})
+       set(Clang_${_comp}_FOUND TRUE)
+    endif()
+  endforeach()
+else()
+  message(ERROR "No Clang components given")
+endif()
 
-# Clang shared library provides just the limited C interface, so it
-# can not be used.  We look for the static libraries.
-FIND_AND_ADD_CLANG_LIB(clangFrontend)
-#FIND_AND_ADD_CLANG_LIB(clangCodeGen)
-#FIND_AND_ADD_CLANG_LIB(clangIndex)
-FIND_AND_ADD_CLANG_LIB(clangSerialization)
-FIND_AND_ADD_CLANG_LIB(clangDriver)
-FIND_AND_ADD_CLANG_LIB(clangTooling)
-FIND_AND_ADD_CLANG_LIB(clangToolingCore)
-FIND_AND_ADD_CLANG_LIB(clangParse)
-FIND_AND_ADD_CLANG_LIB(clangSema)
-#FIND_AND_ADD_CLANG_LIB(clangStaticAnalyzerFrontend)
-#FIND_AND_ADD_CLANG_LIB(clangStaticAnalyzerCheckers)
-#FIND_AND_ADD_CLANG_LIB(clangStaticAnalyzerCore)
-FIND_AND_ADD_CLANG_LIB(clangAnalysis)
-FIND_AND_ADD_CLANG_LIB(clangRewriteFrontend)
-FIND_AND_ADD_CLANG_LIB(clangRewrite)
-FIND_AND_ADD_CLANG_LIB(clangEdit)
-FIND_AND_ADD_CLANG_LIB(clangAST)
-FIND_AND_ADD_CLANG_LIB(clangASTMatchers)
-FIND_AND_ADD_CLANG_LIB(clangLex)
-FIND_AND_ADD_CLANG_LIB(clangBasic)
-#FIND_AND_ADD_CLANG_LIB(clangFormat)
+# Clang_INCLUDE_DIRS
+find_path(Clang_INCLUDE_DIRS clang/Basic/Version.h HINTS ${LLVM_INCLUDE_DIRS})
 
-find_path(CLANG_INCLUDE_DIRS clang/Basic/Version.h HINTS ${LLVM_INCLUDE_DIRS})
-
-if (CLANG_LIBS AND CLANG_INCLUDE_DIRS)
-  MESSAGE(STATUS "Clang libs:  ${CLANG_LIBS}")
-  set(CLANG_FOUND TRUE)
-endif (CLANG_LIBS AND CLANG_INCLUDE_DIRS)
-
-if (CLANG_FOUND)
-  message(STATUS "Found Clang: ${CLANG_INCLUDE_DIRS}")
-else (CLANG_FOUND)
-  if (CLANG_FIND_REQUIRED)
-    message(FATAL_ERROR "Could NOT find Clang")
-  endif (CLANG_FIND_REQUIRED)
-endif (CLANG_FOUND)
-
-endif (NOT LLVM_INCLUDE_DIRS OR NOT LLVM_LIBRARY_DIRS)
+# FIND_PACKAGE_HANDLE_STANDARD_ARGS
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+  Clang
+  REQUIRED_VARS Clang_INCLUDE_DIRS Clang_LIBS
+  VERSION_VAR LLVM_VERSION
+  HANDLE_COMPONENTS
+)
--- cmake/FindLLVM.cmake
+++ cmake/FindLLVM.cmake
@@ -9,51 +9,76 @@
 #  LLVM_LDFLAGS      - llvm linker flags
 #  LLVM_MODULE_LIBS  - list of llvm libs for working with modules.
 
-find_program(LLVM_CONFIG_EXECUTABLE llvm-config DOC "llvm-config executable")
+include(FindPackageHandleStandardArgs)
+
+# known versions of llvm
+set(_LLVM_KNOWN_VERSIONS 
+  ${LLVM_FIND_VERSION}
+  ${LLVM_ADDITIONAL_VERSIONS} 
+  "3.7" "3.6" "3.5" "3.4"
+)
+
+# generate names for llvm-config
+set(_LLVM_CONFIG_NAMES)
+foreach (_version ${_LLVM_KNOWN_VERSIONS})
+  list(APPEND _LLVM_CONFIG_NAMES "llvm-config-${_version}")
+endforeach()
+list(APPEND _LLVM_CONFIG_NAMES "llvm-config")
+
+# find llvm-config
+find_program(LLVM_CONFIG_EXECUTABLE ${_LLVM_CONFIG_NAMES} DOC "llvm-config executable")
 
 if (LLVM_CONFIG_EXECUTABLE)
-  message(STATUS "LLVM llvm-config found at: ${LLVM_CONFIG_EXECUTABLE}")
+  message(STATUS "LLVM `llvm-config` found at: ${LLVM_CONFIG_EXECUTABLE}")
 else (LLVM_CONFIG_EXECUTABLE)
-  message(FATAL_ERROR "Could NOT find LLVM executable")
+  message(FATAL_ERROR "Could NOT find `llvm-config` executable")
 endif (LLVM_CONFIG_EXECUTABLE)
 
+# version
 execute_process(
   COMMAND ${LLVM_CONFIG_EXECUTABLE} --version
   OUTPUT_VARIABLE LLVM_VERSION
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-
 string(REGEX REPLACE "^([0-9]+)\\.([0-9]+).*" "\\1" LLVM_VERSION_MAJOR
 	     "${LLVM_VERSION}")
-
 string(REGEX REPLACE "^([0-9]+)\\.([0-9]+).*" "\\2" LLVM_VERSION_MINOR
 	     "${LLVM_VERSION}")
+	     
+# prefix / bindir
+execute_process(
+  COMMAND ${LLVM_CONFIG_EXECUTABLE} --prefix
+  OUTPUT_VARIABLE LLVM_PREFIX
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+) 
+execute_process(
+  COMMAND ${LLVM_CONFIG_EXECUTABLE} --bindir
+  OUTPUT_VARIABLE LLVM_BINDIR
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)  
 
+# includedir
 execute_process(
   COMMAND ${LLVM_CONFIG_EXECUTABLE} --includedir
   OUTPUT_VARIABLE LLVM_INCLUDE_DIRS
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
+# libdir
 execute_process(
   COMMAND ${LLVM_CONFIG_EXECUTABLE} --libdir
   OUTPUT_VARIABLE LLVM_LIBRARY_DIRS
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
+# cppflags
 execute_process(
   COMMAND ${LLVM_CONFIG_EXECUTABLE} --cppflags
   OUTPUT_VARIABLE LLVM_CFLAGS
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-if (LLVM_CFLAGS MATCHES "\\-DNDEBUG")
-    set(LLVM_WITH_NDEBUG TRUE)
-else (LLVM_CFLAGS MATCHES "\\-DNDEBUG")
-    set(LLVM_WITH_NDEBUG FALSE)
-endif (LLVM_CFLAGS MATCHES "\\-DNDEBUG")
-
-
+# libs
 find_library(LLVM_MODULE_LIBS LLVM-${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR} ${LLVM_LIBRARY_DIRS})
 if (NOT LLVM_MODULE_LIBS)
   execute_process(
@@ -61,14 +86,18 @@ if (NOT LLVM_MODULE_LIBS)
     OUTPUT_VARIABLE LLVM_MODULE_LIBS
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
-endif (NOT LLVM_MODULE_LIBS)
+endif()
 
+# ldflags
 execute_process(
   COMMAND ${LLVM_CONFIG_EXECUTABLE} --ldflags
   OUTPUT_VARIABLE LLVM_LDFLAGS
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-if (LLVM_CONFIG_EXECUTABLE)
-  set(LLVM_FOUND TRUE)
-endif (LLVM_CONFIG_EXECUTABLE)
+# LLVM_FOUND
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+  LLVM
+  REQUIRED_VARS LLVM_CONFIG_EXECUTABLE LLVM_INCLUDE_DIRS LLVM_MODULE_LIBS LLVM_LDFLAGS
+  VERSION_VAR LLVM_VERSION
+)
